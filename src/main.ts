4948d61d206aff50d5c670884d8b6cfd37aff7ef
souheyeb@gmail.com <souheyeb@gmail.com>
2025-03-23 19:34:10 +0100

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
